dc3b5ee7facfdba64afa35dc52414f85fcfb7c2b
mwanawabangona <ellisallday@gmail.com>
2021-12-03 10:35:14 -0800

    Updates

---
+++ precache-manifest.2f6dca851e8a396d2db8789944ac2623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28351bc092dcd7b959fedcaacf227101",
+    "url": "/Math-magician/index.html"
+  },
+  {
+    "revision": "181d84b10f73160e5a68",
+    "url": "/Math-magician/static/css/main.8fb0c84c.chunk.css"
+  },
+  {
+    "revision": "c62a9a5292f485d3f3c7",
+    "url": "/Math-magician/static/js/2.1677133d.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/Math-magician/static/js/2.1677133d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181d84b10f73160e5a68",
+    "url": "/Math-magician/static/js/main.c0123635.chunk.js"
+  },
+  {
+    "revision": "622ed1bbe0bd3746b349",
+    "url": "/Math-magician/static/js/runtime-main.67f78b23.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b90ba34bb538cbded2788cac67df986.js"
+  "/Math-magician/precache-manifest.2f6dca851e8a396d2db8789944ac2623.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Math-magician/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
